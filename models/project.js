122ed83b47bb238f1bc157686e58dc00ab795739
Daniel Radosa <danysko5@gmail.com>
2022-03-08 13:41:01 +0100

    User & Project models
    Most likely the structure is okay.

--- models/project.js
+++ models/project.js
@@ -2,11 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let projectSchema = new Schema ({
-	projectName: { type: String },
-	projectDescription: { type: String },
-	createdBy: { type: String },
-	dateCreated: { type: Date, default: Date.now },
-	deadline: { type: Date }
+	title: { type: String, required: true, min: 3, max: 255 },
+	description: { type: String, required: false, min: 10, max: 255 },
+	deadline: { type: Date, required: true },
+	author: { type: String, required: true },
+	associates: { type: Array, required: false },
+	tasks: [{
+		todo: [{
+			taskTitle: { type: String, required: true, min: 3, max: 255 },
+			status: { type: Boolean, required: true },
+			items: [{
+				singleItem: { type: String, required: true, min: 3, max: 255 }
+			}]
+		}],
+		notes: [{
+			noteTitle: { type: String, required: true, min: 3, max: 255 },
+			content: { type: String, required: true }
+		}]
+	}]
 })
 
 module.exports = mongoose.model("project", projectSchema);
--- models/user.js
+++ models/user.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let userSchema = new Schema ({
+	name: { type: String, required: true, min: 2, max: 255 },
 	email: { type: String, required: true, min: 6, max: 255 },
 	password: { type: String, required: true, min: 6, max: 255 },
-	name: { type: String, required: true, min: 2, max: 255 }
+	projects: [{ 
+		singleProject: { type: String, required: false, min: 3, max: 255 }
+	 }]
 })
 
 module.exports = mongoose.model("user", userSchema);
