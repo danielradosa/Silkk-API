c0e0b8edfe174085d1ba07bd9beaef0aa94d958b
Daniel Radosa <danysko5@gmail.com>
2022-03-19 13:23:11 +0100

    Update project.js

--- routes/project.js
+++ routes/project.js
@@ -1,35 +1,45 @@
 const router = require("express").Router();
-const projects = require("../models/project");
+const project = require("../models/project");
 const { verifyToken } = require("../validation");
 
 // Create a new project
 router.post("/create", verifyToken, (req, res) => {
     data = req.body;
-    projects.insertMany(data)
+    project.insertMany(data)
+    .then(data => { res.send(data); })
+    .catch(err => { res.status(500).send({ message: err.message }); })
+});
+
+// Get all projects ever
+router.get("/all", (req, res) => {
+    data = req.body;
+    project.find(data)
     .then(data => { res.send(data); })
     .catch(err => { res.status(500).send({ message: err.message }); })
 });
 
 // Get specific project
 router.get("/:id", (req, res) => {
-    projects.findById(req.params.id)
+    project.findById(req.params.id)
     .then(data => { res.send(data); })
     .catch(err => { res.status(500).send({ message: err.message }); })
 });
 
 // Get all projects by author
-router.get("/author/:name", verifyToken, (req, res) => {
-    data = req.body.userProjects;
-    projects.find({ author: this.name })
+router.get("/all/:author", verifyToken, (req, res) => {
+    data = req.params.author;
+    project.findOne({ author: data })
     .then(data => { res.send(data); })
     .catch(err => { res.status(500).send({ message: err.message }); })
 });
 
-// Get all favourite projects
-router.get("/favourite/:id", (res) => {
-    projects.find({ favourite: true })
+// Get all favourite projects by author
+router.get("/favourite/:author", verifyToken, (req, res) => {
+    data = req.params.author;
+    project.find({ author: data }, {favourite: true})
     .then(data => { res.send(data); })
     .catch(err => { res.status(500).send({ message: err.message }); })
 });
+
 
 module.exports = router;
