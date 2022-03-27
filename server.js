5474d12593da9068bcc9d2c2cb17889374957a59
Daniel Radosa <danysko5@gmail.com>
2022-03-27 19:36:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-*");
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "auth-token, Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
 });
 
