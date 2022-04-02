a873c168a5e82c852cdb75361d55aaae7ebcead0
Daniel Radosa <danysko5@gmail.com>
2022-04-02 12:06:51 +0200

    Update project.js

--- models/project.js
+++ models/project.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 let projectSchema = new Schema ({
 	title: { type: String, required: true, min: 3, max: 255 },
-	favourite: { type: Boolean, required: false },
-	description: { type: String, required: false, min: 10, max: 255 },
+	favourite: { type: Boolean, required: true },
+	description: { type: String, required: true, min: 10, max: 255 },
 	deadline: { type: String, required: true },
 	author: { type: String, required: false },
 	authorEmail: { type: String, required: true },
